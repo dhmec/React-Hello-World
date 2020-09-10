239af75dff5d7f5af1545ea36cb34208f0be56ac
dhmec <dharmoradiya@gmail.com>
2020-09-10 11:09:43 -0600

    added Hello world compionents

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'; //we'll need React to build our components.
+
+function Hello ()
+{
+    return (
+        <h1>
+            Hello,World!
+        </h1>
+    );
+}
+
+export default Hello;
